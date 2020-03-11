40a312bc2a723b2c87d8c1ba143457956813f7a2
Jeanette Gonzalez <jeanettegonzalezlovesu@gmail.com>
2020-03-11 02:14:30 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -2,8 +2,8 @@ require_relative '../current_age_for_birth_year.rb'
 
 describe "current_age_for_birth_year method"
 
-    age_of_person = current_age_for_birth_year(1984)
+    age_of_person = current_age_for_birth_year(1984) do
+
+    end
 
     expect(age_of_person).to eq(19)
-  end
-  
