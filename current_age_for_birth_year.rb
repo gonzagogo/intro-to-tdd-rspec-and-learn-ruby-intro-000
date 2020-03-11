b56a93cfc57c049ff3d541d32f4563f559ef8f84
Jeanette Gonzalez <jeanettegonzalezlovesu@gmail.com>
2020-03-11 02:01:14 +0000

    Automatically backed up by Learn

---
+++ current_age_for_birth_year.rb
@@ -0,0 +1,8 @@
+require_relative '../current_age_for_birth_year.rb'
+
+describe "current_age_for_birth_year method"
+
+    age_of_person = current_age_for_birth_year(1984)
+
+    expect(age_of_person).to eq(19)
+  end
